2bda0230401e7d27688e26534c43f6842138897b
Martin Hladil <martin@hladil.name>
2010-07-02 15:44:08 +0200

    refresh is working

--- sproutcore/apps/device_hub/controllers/devices.js
+++ sproutcore/apps/device_hub/controllers/devices.js
@@ -14,11 +14,18 @@ DeviceHub.devicesController = SC.ArrayController.create(
 /** @scope DeviceHub.devicesController.prototype */
 {
 
-  selectionDidChange: function() {
-	}.observes('selection'),
-	
-	refreshDevices: function() {
-	  alert("refresh!");
-	}
+  selectionDidChange: function() {}.observes('selection'),
+
+  loadDevices: function() {
+    var devices = DeviceHub.store.find(DeviceHub.DEVICES_QUERY);
+    var packets = DeviceHub.store.find(DeviceHub.PACKETS_QUERY);
+    DeviceHub.devicesController.set("content", devices);
+		DeviceHub.devicesController.set("packets", packets);
+  },
+
+  refreshDevices: function() {
+		this.get("content").refresh();
+		this.get("packets").refresh();
+  }
 
 });
