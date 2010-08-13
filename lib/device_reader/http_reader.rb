b5e36191e4c13d2d51877b1caecf563529589742
Martin Hladil <martin@hladil.name>
2010-08-13 12:46:24 +0200

    device read delay fix & hope

--- lib/device_reader/http_reader.rb
+++ lib/device_reader/http_reader.rb
@@ -3,25 +3,32 @@ module DeviceReader
   class HttpReader
 
     READ_DELAY = 1000 # ms
+    READ_DELAY_SECONDS = READ_DELAY / 1000.0
 
     attr_accessor :ip_address
 
     def initialize(ip_address)
       @ip_address = ip_address
-      @last_read_at = 0
+      @last_read_at = Time.now - READ_DELAY_SECONDS
     end
 
     def raw_read(log_address)
-      if (Time.now - @last_read_at) < (READ_DELAY / 1000.0)
-        sleep((READ_DELAY / 1000.0) - (Time.now - @last_read_at))
-      end
-      data = open("http://#{@ip_address}/logy.txt?A35=#{log_address.to_address}").read
-      @last_read_at = Time.now
-      return data
+      read_delay
+      open("http://#{@ip_address}/logy.txt?A35=#{log_address.to_address}").read
     end
 
     def raw_status
+      read_delay
       open("http://#{@ip_address}/sw1.txt").read
+    end
+
+    private
+
+    def read_delay
+      if (Time.now - @last_read_at) < (READ_DELAY_SECONDS)
+        sleep((READ_DELAY_SECONDS) - (Time.now - @last_read_at))
+      end
+      @last_read_at = Time.now
     end
 
   end
